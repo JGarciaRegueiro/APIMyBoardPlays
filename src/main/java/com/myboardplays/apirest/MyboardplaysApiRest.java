0b444976496248c188b113f54acf8342c6f9414b
DanielJimenezBallesteros <danijb_10@hotmail.com>
2023-05-24 18:44:28 +0200

    cambios

--- src/main/java/com/myboardplays/apirest/MyboardplaysApiRest.java
+++ src/main/java/com/myboardplays/apirest/MyboardplaysApiRest.java
@@ -112,7 +112,7 @@ public class MyboardplaysApiRest {
 		return ResponseEntity.ok(null);
 		}
 	
-	//prueba 1
+	//prueba 2
 	
 	@DeleteMapping("/juego/eliminar/{idJuego}")
 	public ResponseEntity<String> eliminarJuego(@PathVariable int idJuego) {
